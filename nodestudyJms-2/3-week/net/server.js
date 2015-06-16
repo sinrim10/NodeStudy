8ecabc7a87cc9c501823d6e98ecb48507268e23a
Jung Myung Sung <kkforgg@naver.com>
2015-06-16 09:50:01 +0900

    net 모듈 진행

---
+++ nodestudyJms-2/3-week/net/server.js
@@ -0,0 +1,25 @@
+/**
+ * New node file
+ */
+
+var net = require('net');
+var server = net.createServer(function (c){
+	
+	console.log('server connected');
+	
+	c.on('end',function(){
+		
+		console.log('server disconnected');
+		
+	})
+	
+	c.write('hello\r\n');
+	c.pipe(c);
+	
+});
+
+server.listen(8124,function(){
+	
+	console.log('server bound');
+	
+})
